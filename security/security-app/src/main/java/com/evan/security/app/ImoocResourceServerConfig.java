6eeadb575d640c733c8b5a7cf98c5bda9c80b556
Evan Huang <evan.huang@oocl.com>
2019-10-15 19:03:45 +0800

    6-4 重构用户名密码登录

--- security/security-app/src/main/java/com/evan/security/app/ImoocResourceServerConfig.java
+++ security/security-app/src/main/java/com/evan/security/app/ImoocResourceServerConfig.java
@@ -3,8 +3,18 @@
  */
 package com.evan.security.app;
 
+import com.evan.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.evan.security.core.properties.SecurityConstants;
+import com.evan.security.core.properties.SecurityProperties;
+import com.evan.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * @author Evan Huang
@@ -12,6 +22,56 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class ImoocResourceServerConfig {
+public class ImoocResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer imoocSocialSecurityConfig;
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(imoocAuthenticationSuccessHandler)
+                .failureHandler(imoocAuthenticationFailureHandler);
+
+        http//.apply(validateCodeSecurityConfig)
+                //	.and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(imoocSocialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+                        securityProperties.getBrowser().getSignUpUrl(),
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        securityProperties.getBrowser().getSignOutUrl(),
+                        "/user/regist")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 
 }
