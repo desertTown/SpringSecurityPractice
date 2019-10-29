4eb6c4027783cb42e78c9d4ba48e3d5f76b5eb44
Evan Huang <evan.huang@oocl.com>
2019-10-29 21:13:09 +0800

    测试app项目成功(在谷歌插件上测试，社交登录那块没测)

--- security/security-core/src/main/java/com/evan/security/core/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
+++ security/security-core/src/main/java/com/evan/security/core/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
@@ -12,11 +12,7 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 /**
  * @author Evan Huang
@@ -34,8 +30,8 @@ public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapt
 	@Autowired
 	private UserDetailsService userDetailsService;
 
-	@Autowired
-	private PersistentTokenRepository persistentTokenRepository;
+//	@Autowired
+//	private PersistentTokenRepository persistentTokenRepository;
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
@@ -44,9 +40,9 @@ public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapt
 		smsCodeAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
 		smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(imoocAuthenticationSuccessHandler);
 		smsCodeAuthenticationFilter.setAuthenticationFailureHandler(imoocAuthenticationFailureHandler);
-
-		String key = UUID.randomUUID().toString();
-		smsCodeAuthenticationFilter.setRememberMeServices(new PersistentTokenBasedRememberMeServices(key, userDetailsService, persistentTokenRepository));
+		// app环境下没有记住功能
+//		String key = UUID.randomUUID().toString();
+//		smsCodeAuthenticationFilter.setRememberMeServices(new PersistentTokenBasedRememberMeServices(key, userDetailsService, persistentTokenRepository));
 
 
 		SmsCodeAuthenticationProvider smsCodeAuthenticationProvider = new SmsCodeAuthenticationProvider();
--- security/security-demo/src/main/java/com/evan/security/MyUserDetailsService.java
+++ security/security-demo/src/main/java/com/evan/security/MyUserDetailsService.java
@@ -52,6 +52,6 @@ public class MyUserDetailsService implements UserDetailsService, SocialUserDetai
 		logger.info("数据库密码是:"+password);
 		return new SocialUser(userId, password,
 				true, true, true, true,
-				AuthorityUtils.commaSeparatedStringToAuthorityList("xxx"));
+				AuthorityUtils.commaSeparatedStringToAuthorityList("admin,ROLE_USER"));
 	}
 }
